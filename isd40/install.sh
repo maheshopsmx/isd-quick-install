8c1d176443e953d7684c173e3257b99d7f0fa943
maheshopsmx <mahesh.kota@opsmx.io>
2022-10-13 13:14:34 +0530

    Create install.sh

---
+++ isd40/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+kubectl create namespace opsmx-isd
+kubectl -n opsmx-isd apply -f https://raw.githubusercontent.com/OpsMx/isd-quick-install/main/isd40/isd-gitea-quick.yaml
+while [ "$(kubectl get pods -n opsmx-isd -o jsonpath='{.items[*].status.containerStatuses[0].ready}')" != "true" ]; do
+   sleep 5
+   echo "Waiting for Broker to be ready."
+   if lsof -Pi :8080 -sTCP:LISTEN -t >/dev/null ; then
+    echo "running"
+    kubectl -n opsmx-isd port-forward svc/oes-ui 8089
+    else
+    echo "not running"
+    kubectl -n opsmx-isd port-forward svc/oes-ui 8080
+    fi
+done
+
